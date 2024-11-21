e7d3c82dacfb322f43d49d0efdfbd35ecfcbc973
MoChahadeh <applednd2002@gmail.com>
2024-11-21 12:13:22 +0100

    Day 3: We controlled the hardware with a node through serial connection

---
+++ src/stm_station/stm_station/CommandPublisher.py
@@ -0,0 +1,55 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import Float32MultiArray
+from random import random, randint
+
+class CommandPublisher(Node):
+    
+    def __init__(self):
+        super().__init__("stm_command_publisher")
+        
+        
+        self.command_publisher = self.create_publisher(Float32MultiArray, "control_commands", 10)
+        
+        self.timer = self.create_timer(5, self.timer_callback)
+
+        
+    
+    def publish_data(self, msg:Float32MultiArray):
+        
+        self.command_publisher.publish(msg=msg)
+        self.get_logger().info("Publishing: "+str(msg.data))
+        
+        
+    def timer_callback(self):
+        
+        msg = Float32MultiArray()
+        
+        # format: {control_type, goal_position, Kp, PWM}
+        control_type = float(randint(0,2))
+        goal_position = float(randint(0, 250)) if control_type == 1 else float(0)
+        Kp = float(random()*5) if control_type == 1 else float(0)
+        pwm = float(randint(-250, 250)) if control_type == 2 else float(0)
+        
+        msg.data = [control_type, goal_position, Kp, pwm]
+        
+        print(msg.data)
+        
+        self.publish_data(msg)
+        
+        
+def main(args=None):
+    rclpy.init(args=args)
+    node = CommandPublisher()
+    try:
+        rclpy.spin(node)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        node.destroy_node()
+        rclpy.shutdown()
+
+if __name__ == '__main__':
+    main()
+
+        
